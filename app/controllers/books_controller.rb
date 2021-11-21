3abf724896f36bbfad4f8d4adef6dc91468697b2
EC2 Default User <ec2-user@ip-172-31-8-88.ec2.internal>
2021-11-21 09:00:27 +0000

    [Update]indexページの作成　createのアソシエーションがうまく定義できていない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,27 @@
 class BooksController < ApplicationController
     
     def index
+        @books=Book.all
+        @book=Book.new
+    end
+    
+    def create
+        @book=Book.new(book_params)
+        @book.user_id == current_user.id
+        if @book.save
+            flash[:notice]="Book was successfully created."
+            redirect_to book_path(@book)
+        else
+            @books=Book.all
+            render :index
+        end
     end
     
     def show
     
     end
     
-    def create
-    end
+    
     
     def edit
     end
@@ -18,4 +31,12 @@ class BooksController < ApplicationController
     
     def destroy
     end
+    
+    private
+    
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
+    
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,25 @@
 class UsersController < ApplicationController
     def index
+        @users=User.all
+        @book=Book.new
     end
     
     def show
+        @user=User.find(params[:id])
+        @book=Book.new
+        @books=@user.books.all
+       
     end
+    
     
     def edit
     end
     
     def update
+    end
+    
+    private
+    def user_params
+        params.require(:user).permit(:name, :profile_image, :introduction)
     end
 end
